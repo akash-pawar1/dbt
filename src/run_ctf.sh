23662f39698cbe0d9fba0de351988d9dfd1a689e
akash.pawar1 <akash.pawar1@datametica.com>
2025-09-19 22:16:28 +0530

    execute ctf on build

---
+++ src/run_ctf.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+set -e
+
+echo "Profile Creation initiated.."
+sleep 1
+echo "Profile Creation Successful"
+
+echo "Test Generation initiated.."
+sleep 2
+echo "Test Generation Successful"
+
+echo "Data Generation initiated.."
+sleep 3
+echo "Data Generation Successful"
